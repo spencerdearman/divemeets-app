7c2ccc722a68e6a1f942c1e65763e0b4accf55e6
Logan Sherwin <lsherwin10@gmail.com>
2023-03-02 13:49:50 -0600

    Redesign Meets page to use a ScrollView

--- DiveMeets/DiveMeets/Views/MeetList.swift
+++ DiveMeets/DiveMeets/Views/MeetList.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct MeetList: View {
+    private let frameWidth: CGFloat = 350
+    private let frameHeight: CGFloat = 50
+    private let cornerRadius: CGFloat = 15
+    private let rowSpacing: CGFloat = 3
+    private let rowColor: Color = Color.white
+    private let textColor: Color = Color.black
+    private let fontSize: CGFloat = 20
+    private let grayValue: CGFloat = 0.95
+    
     var body: some View {
         NavigationView {
-            List(meets) { meet in
-                NavigationLink {
-                    MeetPage(meetInstance: meet)
-                } label: {
-                    MeetElement(meet0: meet)
+            ZStack {
+                Color(red: grayValue, green: grayValue, blue: grayValue)
+                    .ignoresSafeArea()
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: rowSpacing) {
+                        ForEach(meets) { meet in
+                            NavigationLink(destination: MeetPage(meetInstance: meet)) {
+                                GeometryReader { geometry in
+                                    HStack {
+                                        MeetElement(meet0: meet)
+                                            .foregroundColor(textColor)
+                                            .font(.system(size: fontSize))
+                                            .padding()
+                                        Spacer()
+                                        Image(systemName: "chevron.right")
+                                            .foregroundColor(Color.gray)
+                                            .padding()
+                                    }
+                                    .frame(width: frameWidth, height: frameHeight)
+                                    .background(rowColor)
+                                    .cornerRadius(cornerRadius)
+                                }
+                                .frame(width: frameWidth, height: frameHeight)
+                            }
+                        }
+                    }
                 }
+                .navigationTitle("Meets")
             }
-            .navigationTitle("Meets")
         }
     }
 }
