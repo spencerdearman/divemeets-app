e6a458e7d45348a4df060fccd9154908c73ad85b
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-03-27 00:01:39 -0500

    updated, now working however need to edit the meet list to only include actual meets from the array

--- DiveMeets/DiveMeets/Views/MeetList.swift
+++ DiveMeets/DiveMeets/Views/MeetList.swift
@@ -43,9 +43,9 @@ struct MeetList: View {
             if firstCol.contains(".") {
                 // Get the event title
                 let eventTitle = firstCol.components(separatedBy: " - ")[0]
-                
                 // Get the place and score
-                guard let placeScoreStr = row.last, let placeScoreMatch = try? NSRegularExpression(pattern: "(\\d+)\\s+(\\d+.\\d+)").firstMatch(in: placeScoreStr, options: [], range: NSRange(location: 0, length: placeScoreStr.count)), placeScoreMatch.numberOfRanges == 3, let placeRange = Range(placeScoreMatch.range(at: 1), in: placeScoreStr), let scoreRange = Range(placeScoreMatch.range(at: 2), in: placeScoreStr) else {
+                
+                guard let placeScoreStr = row.last, let placeScoreMatch = try? NSRegularExpression(pattern: "([A-Z\\d]+)\\s+(\\d+.\\d+)").firstMatch(in: placeScoreStr, options: [], range: NSRange(location: 0, length: placeScoreStr.count)), placeScoreMatch.numberOfRanges == 3, let placeRange = Range(placeScoreMatch.range(at: 1), in: placeScoreStr), let scoreRange = Range(placeScoreMatch.range(at: 2), in: placeScoreStr) else {
                     print("Error: Invalid place at index \(row.count - 1)")
                     return nil
                 }
@@ -101,91 +101,3 @@ struct MeetList: View {
         }
     }
 }
-
-
-/*
-import SwiftUI
-
-struct MeetList: View {
-    @Environment(\.colorScheme) var currentMode
-    var profileLink: String
-    @State var diverData : [Array<String>] = []
-    @State var offset: CGFloat = 0
-    @State var lastOffset: CGFloat = 0
-    @Binding var hideTabBar: Bool
-    @StateObject private var parser = HTMLParser()
-    
-    
-    /// Style adjustments for elements of list
-    private let frameWidth: CGFloat = 350
-    private let frameHeight: CGFloat = 50
-    private let cornerRadius: CGFloat = 30
-    private let rowSpacing: CGFloat = 3
-    private let fontSize: CGFloat = 20
-    let startIndex = 2
-    
-    
-    func createMeets(data: [[String]]) -> [Meet]? {
-        var meets = [Meet]()
-        var currentMeetName = ""
-        var currentMeetEvents = [String]()
-        var currentMeetPlaces = [Int]()
-        var currentMeetScores = [Double]()
-        
-        for row in data {
-            let firstCol = row[0]
-            
-            // If the first column doesn't contain a number, it's a meet or event name
-            if firstCol.rangeOfCharacter(from: CharacterSet.decimalDigits) == nil {
-                // Save the previous meet's data
-                if !currentMeetName.isEmpty {
-                    let meet = Meet(meetName: currentMeetName, meetEvents: currentMeetEvents, meetPlaces: currentMeetPlaces, meetScores: currentMeetScores)
-                    meets.append(meet)
-                    currentMeetEvents.removeAll()
-                    currentMeetPlaces.removeAll()
-                    currentMeetScores.removeAll()
-                }
-                
-                // Set the current meet name
-                currentMeetName = row.joined(separator: " ")
-            } else if let range = firstCol.range(of: #"20\d{2}"#, options: .regularExpression) {
-                currentMeetName = String(firstCol[..<range.lowerBound]).trimmingCharacters(in: .whitespaces)
-                if !currentMeetName.isEmpty {
-                    let meet = Meet(meetName: currentMeetName, meetEvents: currentMeetEvents, meetPlaces: currentMeetPlaces, meetScores: currentMeetScores)
-                    meets.append(meet)
-                    currentMeetEvents.removeAll()
-                    currentMeetPlaces.removeAll()
-                    currentMeetScores.removeAll()
-                }
-                // Get the year
-                let year = String(firstCol[range])
-                // Set the current meet name to include the year
-                currentMeetName = "\(currentMeetName) \(year)"
-            } else {
-                // Otherwise, it's an event result
-                // Get the event title
-                let eventTitle = firstCol.components(separatedBy: " - ")[0]
-                
-                // Get the place and score
-                guard let placeScoreStr = row.last, let placeScoreMatch = try? NSRegularExpression(pattern: "(\\d+)\\s+(\\d+.\\d+)").firstMatch(in: placeScoreStr, options: [], range: NSRange(location: 0, length: placeScoreStr.count)), placeScoreMatch.numberOfRanges == 3, let placeRange = Range(placeScoreMatch.range(at: 1), in: placeScoreStr), let scoreRange = Range(placeScoreMatch.range(at: 2), in: placeScoreStr) else {
-                    print("Error: Invalid place at index \(row.count - 1)")
-                    return nil
-                }
-                
-                let place = Int(placeScoreStr[placeRange]) ?? 0
-                let score = Double(placeScoreStr[scoreRange]) ?? 0.0
-                
-                currentMeetEvents.append(eventTitle)
-                currentMeetPlaces.append(place)
-                currentMeetScores.append(score)
-            }
-        }
-        
-        // Save the last meet's data
-        if !currentMeetName.isEmpty {
-            let meet = Meet(meetName: currentMeetName, meetEvents: currentMeetEvents, meetPlaces: currentMeetPlaces, meetScores: currentMeetScores)
-            meets.append(meet)
-        }
-        return meets
-    }
-*/
