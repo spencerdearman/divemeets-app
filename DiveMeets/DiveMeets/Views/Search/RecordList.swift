da7cde460b54d39c4984ece24ca06b25d34195a7
Logan Sherwin <lsherwin10@gmail.com>
2023-04-27 20:25:34 -0500

    Update sizing dynamically in Search View

--- DiveMeets/DiveMeets/Views/Search/RecordList.swift
+++ DiveMeets/DiveMeets/Views/Search/RecordList.swift
@@ -11,17 +11,11 @@ struct RecordList: View {
     @Environment(\.colorScheme) var currentMode
     @Binding var records: [String: String]
     @Binding var resultSelected: Bool
-    @State var offset: CGFloat = 0
-    @State var lastOffset: CGFloat = 0
     
     // Style adjustments for elements of list
-    private let frameWidth: CGFloat = 350
-    private let frameHeight: CGFloat = 50
     private let cornerRadius: CGFloat = 30
     private let rowSpacing: CGFloat = 3
     private let textColor: Color = Color.primary
-    private let fontSize: CGFloat = 20
-    private let grayValue: CGFloat = 0.95
     
     private var rowColor: Color {
         currentMode == .light ? Color.white : Color.black
@@ -46,11 +40,10 @@ struct RecordList: View {
                                 destination: ProfileView(
                                     link: value,
                                     diverID: String(value.utf16.dropFirst(67)) ?? "")) {
-                                        GeometryReader { geometry in
                                             HStack {
                                                 Text(key)
                                                     .foregroundColor(textColor)
-                                                    .font(.system(size: fontSize))
+                                                    .font(.title3)
                                                     .padding()
                                                 
                                                 Spacer()
@@ -59,13 +52,8 @@ struct RecordList: View {
                                                     .foregroundColor(Color.gray)
                                                     .padding()
                                             }
-                                            .frame(width: frameWidth,
-                                                   height: frameHeight)
                                             .background(rowColor)
                                             .cornerRadius(cornerRadius)
-                                        }
-                                        .frame(width: frameWidth,
-                                               height: frameHeight)
                                         .onDisappear {
                                             resultSelected = true
                                         }
@@ -73,6 +61,7 @@ struct RecordList: View {
                                             resultSelected = false
                                         }
                                     }
+                                    .padding([.leading, .trailing])
                         }
                     }
                     .padding()
