0bdb790e600921d11f0352d649d263e5c7293ff3
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-05-06 12:02:49 -0500

    Working through bugs, parsing working

--- DiveMeets/DiveMeets/Views/Login/EventParser.swift
+++ DiveMeets/DiveMeets/Views/Login/EventParser.swift
@@ -9,19 +9,45 @@ import SwiftUI
 import SwiftSoup
 
 final class EventHTMLParser: ObservableObject {
-    @Published var myData = String()
+    @Published var myData = [String:[String:(String, Double, String)]]()
+    @Published var mainDictionary = [String:[String:(String, Double, String)]]()
+    //@Published var myData = [(String, Int, String)]()
     let getTextModel = GetTextAsyncModel()
     
-    func parse(html: String) async throws -> String {
+    func parse(html: String) async throws -> [String:[String:(String, Double, String)]] {
         let document: Document = try SwiftSoup.parse(html)
         guard let body = document.body() else {
-            return ""
+            return [:]
         }
         let main = try body.getElementsByTag("table")
         print("this is the HTML")
-        let myData = try main[1].getElementsByTag("a").attr("href")
         
-        return myData
+        //Getting the overarching td and then pulling the 3 items within
+        let overall = try main[1].getElementsByTag("tr")
+        var string = [String]()
+        var meetEvent = ""
+        var eventPlace = ""
+        var eventScore = 0.0
+        var eventLink = ""
+        var meetName = ""
+        for (i, t) in overall.enumerated(){
+            var testString = try t.text()
+            if i == 0 {
+                continue
+            }
+            else if testString.contains(".") {
+                meetEvent = try t.getElementsByTag("td")[0].text().replacingOccurrences(of: "  ", with: "")
+                eventPlace = try t.getElementsByTag("td")[1].text()
+                eventScore = Double(try t.getElementsByTag("td")[2].text())!
+                eventLink = try t.getElementsByTag("a").attr("href")
+                string.append(try t.text())
+                mainDictionary[meetName] = [meetEvent: (eventPlace, eventScore, eventLink)]
+            } else {
+                meetName = try t.text()
+                mainDictionary[meetName] = [String:(String, Double, String)]()
+            }
+        }
+        return mainDictionary
     }
     
     
