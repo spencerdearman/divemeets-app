3c94b94fb72032301047ddb4550f424832fc6640
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-05-09 16:28:03 -0500

    Getting all of the new parsers to work, be warned with the amount of new parsing

--- DiveMeets/DiveMeets/Views/Login/MeetList.swift
+++ DiveMeets/DiveMeets/Views/Login/MeetList.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct MeetList: View {
     @Environment(\.colorScheme) var currentMode
     var profileLink: String
-    @State var diverData: [[String]] = []
+    @State var diverData: [Int:[String:[String:(String, Double, String)]]] = [:]
     @State var offset: CGFloat = 0
     @State var lastOffset: CGFloat = 0
     @State var meets: [MeetEvent] = []
-    @StateObject private var parser = HTMLParser()
+    @StateObject private var parser = EventHTMLParser()
     
     // Style adjustments for elements of list
     private let frameWidth: CGFloat = 350
@@ -22,92 +22,37 @@ struct MeetList: View {
     private let cornerRadius: CGFloat = 30
     private let rowSpacing: CGFloat = 10
     private let fontSize: CGFloat = 20
-    let startIndex = 2
     
     private var customGray: Color {
         let gray = currentMode == .light ? 0.95 : 0.1
         return Color(red: gray, green: gray, blue: gray)
     }
     
-    func createMeets(data: [[String]]) -> [MeetEvent]? {
-        if data.count < 2 {
+    func createMeets(data: [Int:[String:[String:(String, Double, String)]]]) -> [MeetEvent]? {
+        if data.count < 1 {
             return nil
         }
         
         var meets = [MeetEvent]()
-        var currentMeetName = ""
-        var currentMeetEvents: [MeetEvent]?
-        var currentMeetPlaces: Int?
-        var currentMeetScores: Double?
+        var currentMeetEvents: [MeetEvent]? = []
         
-        for index in 2..<data.count {
-            let row = data[index]
-            let firstCol = row[0]
-            
-            // If the first column contains a ".", it's a meet event title
-            if firstCol.contains(".") {
-                // Get the event title
-                let eventTitle = firstCol.components(separatedBy: " - ")[0]
-                // Get the place and score
-                
-                guard let placeScoreStr = row.last,
-                      let placeScoreMatch = try? NSRegularExpression(
-                        pattern: "([A-Z\\d]+)\\s+(\\d+.\\d+)")
-                    .firstMatch(in: placeScoreStr, options: [],
-                                range: NSRange(location: 0, length: placeScoreStr.count)),
-                      placeScoreMatch.numberOfRanges == 3,
-                      let placeRange = Range(placeScoreMatch.range(at: 1), in: placeScoreStr),
-                      let scoreRange = Range(placeScoreMatch.range(at: 2), in: placeScoreStr) else {
-                    print("Error: Invalid place at index \(row.count - 1)")
-                    return nil
-                }
-                
-                let place = Int(placeScoreStr[placeRange]) ?? 0
-                let score = Double(placeScoreStr[scoreRange]) ?? 0.0
-                
-                if currentMeetEvents == nil{
+        for i in 1...diverData.count{
+            if let value = diverData[i] {
+                for (name, meetEvent) in value{
+                    for event in meetEvent {
+                        let(place, score, link) = event.value
+                        currentMeetEvents!.append(MeetEvent(name: event.key, place: Int(place), score: score, isChild: true, link: link))
+                    }
+                    let meet = MeetEvent(name: name, children: currentMeetEvents)
+                    meets.append(meet)
                     currentMeetEvents = []
                 }
-                currentMeetEvents!.append(MeetEvent(name: eventTitle, place: place, score: score, isChild: true))
-
-            } else {
-                // If the first column doesn't contain ".", it's a meet name
-                // Save the previous meet's data
-                if !currentMeetName.isEmpty {
-                    let meet = MeetEvent(name: currentMeetName, children: currentMeetEvents)
-                    meets.append(meet)
-                    currentMeetEvents = nil
-                    currentMeetPlaces = nil
-                    currentMeetScores = nil
-                }
-                // Set the current meet name
-                currentMeetName = firstCol
             }
         }
-        
-        // Save the last meet's data
-        if !currentMeetName.isEmpty {
-            let meet = MeetEvent(name: currentMeetName, children: currentMeetEvents)
-            meets.append(meet)
-        }
-        
-        var updated_meets = [MeetEvent]()
-        meets.forEach{ meet in
-            if meet.name == "Dive Statistics" {
-                return
-            }
-            if meet.name == "Dive & Height Description High Score Avg Score # of Times" {
-                return
-            }
-            else {
-                updated_meets.append(meet)
-            }
-        }
-        return updated_meets
+        return meets
     }
     
-    
-    
+
     var body: some View {
         
         ZStack{}
@@ -115,24 +60,20 @@ struct MeetList: View {
                 Task {
                     await parser.parse(urlString: profileLink)
                     diverData = parser.myData
+                    //print(diverData)
                     meets = createMeets(data: diverData) ?? []
-                    print(meets)
-//                                        print(diverData)
-                    //                    print(createMeets(data: diverData) as Any)
+//                    print(meets)
                 }
             }
         
         let rowColor: Color = currentMode == .light
         ? Color.white
         : Color.black
-        
+
         NavigationView {
             ZStack {
                 // Background color for View
                 customGray.ignoresSafeArea()
-                
-//                ScrollView(.vertical, showsIndicators: false) {
-//                    VStack(spacing: rowSpacing) {
                         List($meets, children: \.children) { $meet in
                             (!meet.isChild ?
                              AnyView(
@@ -141,12 +82,9 @@ struct MeetList: View {
                                 childView(meet: $meet)
                              ))
                             .frame(width: frameWidth,
-                                   height: meet.isOpen ? 400: 60)
+                                   height: meet.isOpen ? 400: 45)
                             }
                         }
-//                    }
-//                    .padding()
-//                }
                 .navigationTitle("Meets")
             }
         }
@@ -181,6 +119,7 @@ struct parentView: View{
             .foregroundColor(.primary)
             .padding()
             
+            Spacer()
         }
     }
 }
