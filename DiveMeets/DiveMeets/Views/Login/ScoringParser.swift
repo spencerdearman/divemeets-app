c953ff17973511c04f93dbf2092bba2020c4d57f
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-05-19 01:20:58 -0500

    Adding new parsing function for scores, needs to become async

---
+++ DiveMeets/DiveMeets/Views/Login/ScoringParser.swift
@@ -0,0 +1,50 @@
+//
+//  ScoringParser.swift
+//  DiveMeets
+//
+//  Created by Spencer Dearman on 5/18/23.
+//
+
+import SwiftUI
+import SwiftSoup
+
+final class ScoreHTMLParser: ObservableObject {
+    @Published var scoreData = [Int: Double]()
+    
+    let getTextModel = GetTextAsyncModel()
+    
+    func parse(html: String) async throws -> [Int: Double] {
+        let document: Document = try SwiftSoup.parse(html)
+        guard let body = document.body() else {
+            return [:]
+        }
+        let main = try body.getElementsByTag("tbody")
+        //let scores = try main[0].getElementsByTag("tr")
+        let scores = try main.select("td[style*=color:000000]")
+        for (i, t) in scores.enumerated() {
+            //starting at 1 for the key because matching judge number
+            scoreData[i + 1] = Double(try t.text())
+        }
+        return scoreData
+    }
+    
+    func parse(urlString: String) async {
+        guard let url = URL(string: urlString) else { return }
+        
+        // This sets getTextModel's text field equal to the HTML from url
+        await getTextModel.fetchText(url: url)
+        
+        if let html = getTextModel.text {
+            do {
+                let data = try await parse(html: html)
+                await MainActor.run {
+                    scoreData = data
+                }
+            } catch {
+                print("Error parsing HTML: \(error)")
+            }
+        } else {
+            print("Could not fetch text")
+        }
+    }
+}
