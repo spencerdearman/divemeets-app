498ac1ced6365113500427a7f2ee6d6bf139182e
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-05-19 01:20:58 -0500

    Adding new parsing function for scores, needs to become async

--- DiveMeets/DiveMeets/Views/Login/Event.swift
+++ DiveMeets/DiveMeets/Views/Login/Event.swift
@@ -11,8 +11,10 @@ struct Event: View {
     @Binding var meet: MeetEvent
     @State var diverData : (String, String, String, Double, Double, Double) = ("", "", "", 0.0, 0.0, 0.0)
     @State var diverTableData: [Int: (String, String, String, Double, Double, Double, String)] = [:]
+    @State var scoreData : [Int: Double] = [:]
     
     @StateObject private var parser = EventHTMLParser()
+    @StateObject private var scoreParser = ScoreHTMLParser()
     
     var body: some View {
         ZStack{}
@@ -22,7 +24,9 @@ struct Event: View {
                     diverData = parser.eventData
                     await parser.tableDataParse(urlString: meet.link!)
                     diverTableData = parser.diveTableData
-                    print(diverTableData)
+                    //print(diverTableData)
+                    await scoreParser.parse(urlString: "https://secure.meetcontrol.com/divemeets/system/judgesscoresext.php?meetnum=8698&eventnum=7180&dvrnum=51197&divord=1&eventtype=9&synchdvrnum=&sts=1674154171")
+                    scoreData = scoreParser.scoreData
                 }
             }
         VStack{
