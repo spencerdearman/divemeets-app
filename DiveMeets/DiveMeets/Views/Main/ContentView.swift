b6126ef8df57ec631af6bf3f9b80978849b056ee
Logan Sherwin <lsherwin10@gmail.com>
2023-05-29 00:53:18 -0500

    Early parsing of meet info/results pages

--- DiveMeets/DiveMeets/Views/Main/ContentView.swift
+++ DiveMeets/DiveMeets/Views/Main/ContentView.swift
@@ -43,19 +43,19 @@ struct ContentView: View {
                         HStack {
                             // Add different page views here for different tabs
                             switch tab {
-                            case .house:
-//                                Home()
+                                case .house:
+//                                    Home()
                                     MeetPageView(meetLink: "https://secure.meetcontrol.com/divemeets/system/meetinfoext.php?meetnum=8996")
-                            case .gearshape:
-//                                Image(systemName: tab.rawValue)
-//                                Text("Settings")
-//                                    .bold()
-//                                    .animation(nil, value: selectedTab)
-                                    MeetPageView(meetLink: "https://secure.meetcontrol.com/divemeets/system/meetresultsext.php?meetnum=8958")
-                            case .magnifyingglass:
-                                SearchView(isIndexingMeets: $isIndexingMeets)
-                            case .person:
-                                LoginSearchView()
+                                case .gearshape:
+                                    MeetPageView(meetLink: "https://secure.meetcontrol.com/divemeets/system/meetresultsext.php?meetnum=8996")
+//                                    Image(systemName: tab.rawValue)
+//                                    Text("Settings")
+//                                        .bold()
+//                                        .animation(nil, value: selectedTab)
+                                case .magnifyingglass:
+                                    SearchView(isIndexingMeets: $isIndexingMeets)
+                                case .person:
+                                    LoginSearchView()
                             }
                         }
                         .tag(tab)
