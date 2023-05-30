a733bfb39e5e0ddecb65369bf0ed69d61238c839
Logan Sherwin <lsherwin10@gmail.com>
2023-05-29 20:16:08 -0500

    Get diver and coach list data

--- DiveMeets/DiveMeets/Views/Login/MeetPageView.swift
+++ DiveMeets/DiveMeets/Views/Login/MeetPageView.swift
@@ -18,11 +18,8 @@ struct MeetPageView: View {
     // Only meetEventData OR meetResultsEventData should be nil at a time (event is nil when passed
     //     a results link, and resultsEvent is nil when passed an info link)
     @State private var meetEventData: MeetEventData?
-    @State private var meetResultsEventData: MeetResultsEventData?
     @State private var meetDiverData: MeetDiverData?
     @State private var meetCoachData: MeetCoachData?
-    @State private var meetInfoData: MeetInfoJointData?
-    @State private var meetResultsData: MeetResultsData?
     @ObservedObject private var mpp: MeetPageParser = MeetPageParser()
     private let getTextModel = GetTextAsyncModel()
     var meetLink: String
@@ -149,60 +146,72 @@ struct MeetPageView: View {
         return result
     }
     
+    private func tupleToList(data: MeetDiverData) -> [[String]] {
+        var result: [[String]] = []
+        for diver in data {
+            let name = diver.0
+            let team = diver.1
+            let link = diver.2
+            
+            var row = [name, team, link]
+            for d in diver.3 {
+                row.append(d)
+            }
+            
+            result.append(row)
+        }
+        
+        return result
+    }
+    
+    private func tupleToList(data: MeetCoachData) -> [[String]] {
+        var result: [[String]] = []
+        
+        for coach in data {
+            result.append([coach.0, coach.1, coach.2])
+        }
+        
+        return result
+    }
+    
     var body: some View {
-        VStack {
-            if let meetInfoData = meetInfoData {
-                if showBackButton {
-                    getBackButton()
-                        .padding(.horizontal)
-                }
-                MeetInfoPageView(meetInfoData: meetInfoData)
-                    .onAppear {
-                        print("Info View")
-                    }
-                Spacer()
-            } else if let meetResultsData = meetResultsData {
-                if showBackButton {
-                    getBackButton()
-                        .padding(.horizontal)
-                }
-                MeetResultsPageView(meetResultsData: meetResultsData)
-                    .onAppear {
-                        print("Results View")
-                    }
-                Spacer()
-            } else {
-                VStack {
-                    Text("Getting meet information...")
-                    ProgressView()
+        ZStack {
+            VStack {
+//                ForEach(tupleToList(data: meetEventData ?? []), id: \.self) { elem in
+//                    Text("Date: " + elem[0])
+//                    Text("Event: " + elem[1])
+//                    Text("Name: " + elem[2])
+//                    Text("Rule: " + elem[3])
+//                    Text("Entries: " + elem[4])
+//                }
+                
+//                ForEach(tupleToList(data: meetDiverData ?? []), id: \.self) { elem in
+//                    Text("Name: " + elem[0])
+//                    Text("Team: " + elem[1])
+//                    Text("Link: " + elem[2])
+//                }
+                
+                ForEach(tupleToList(data: meetCoachData ?? []), id: \.self) { elem in
+                    Text("Name: " + elem[0])
+                    Text("Team: " + elem[1])
+                    Text("Link: " + elem[2])
                 }
             }
         }
         .onAppear {
             Task {
-                // Checks first for cached info and results data before parsing
-                if let (info, results) = cachedMeetData[meetLink] {
-                    meetInfoData = info
-                    meetResultsData = results
-                } else {
-                    // Initialize meet parse from index page
-                    let url = URL(string: meetLink)
-                    
-                    if let url = url {
-                        // This sets getTextModel's text field equal to the HTML from url
-                        await getTextModel.fetchText(url: url)
-                        
-                        if let html = getTextModel.text {
-                            meetData = try await mpp.parseMeetPage(link: meetLink, html: html)
-                            if let meetData = meetData {
-                                meetInfoData = await mpp.getMeetInfoData(data: meetData)
-                                meetResultsData = await mpp.getMeetResultsData(data: meetData)
-                                
-                                cachedMeetData[meetLink] = (meetInfoData, meetResultsData)
-                            } else {
-                                print("Meet page failed to parse")
-                            }
-                        }
+                // Initialize meet parse from index page
+                let url = URL(string: meetLink)!
+                
+                // This sets getTextModel's text field equal to the HTML from url
+                await getTextModel.fetchText(url: url)
+                
+                if let html = getTextModel.text {
+                    meetData = try await mpp.parseMeetPage(link: meetLink, html: html)
+                    if meetData != nil {
+                        meetEventData = await mpp.getEventData(data: meetData!)
+                        meetDiverData = mpp.getDiverListData(data: meetData!)
+                        meetCoachData = mpp.getCoachListData(data: meetData!)
                     }
                 }
             }
