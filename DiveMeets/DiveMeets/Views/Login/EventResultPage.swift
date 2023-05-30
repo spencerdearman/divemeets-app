48df7edbbfc445e07e4d6894b2dbff92becb6c1c
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-05-29 19:01:32 -0500

    Adding dynamic sizing

--- DiveMeets/DiveMeets/Views/Login/EventResultPage.swift
+++ DiveMeets/DiveMeets/Views/Login/EventResultPage.swift
@@ -11,7 +11,14 @@ import SwiftUI
 struct EventResultPage: View {
     @StateObject private var parser = EventPageHTMLParser()
     @State var meetLink: String = "https://secure.meetcontrol.com/divemeets/system/eventresultsext.php?meetnum=7984&eventnum=1020&eventtype=9"
-    @State var resultData: [Int: (String, String, String, String, Double, String, String)] = [:]
+    //                     Place  Name   NameLink  Team  TeamLink Score ScoreLink Score Diff.
+    //var resultData: [(Int, String, String, String, String, Double, String, String)] = []
+    @State var resultData: [[String]] = []
+    @ScaledMetric private var maxHeightOffsetScaled: CGFloat = 50
+    
+    private var maxHeightOffset: CGFloat {
+        min(maxHeightOffsetScaled, 90)
+    }
     
     var body: some View {
         ZStack{}
@@ -21,6 +28,66 @@ struct EventResultPage: View {
                     resultData = parser.eventPageData
                 }
             }
-        Text("Hello World")
+        
+        VStack{
+            Text("Event Page")
+            ScalingScrollView(records: resultData) { (elem) in
+                PersonBubbleView(elements: elem)
+            }
+            .padding(.bottom, maxHeightOffset)
+        }
+    }
+}
+
+
+struct PersonBubbleView: View {
+    @Environment(\.colorScheme) var currentMode
+    
+    private var bubbleColor: Color {
+        currentMode == .light ? .white : .black
+    }
+    
+    //  (Place  Name   NameLink  Team  TeamLink Score ScoreLink Score Diff.)
+    private var elements: [String]
+    
+    init(elements: [String]) {
+        self.elements = elements
+    }
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(bubbleColor)
+            VStack {
+                VStack {
+                    Text(elements[1])
+                        .font(.title3)
+                        .bold()
+                        .scaledToFit()
+                        .minimumScaleFactor(0.5)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .lineLimit(1)
+                    Spacer()
+                    HStack{
+                        Text("Place: " + elements[0])
+                        Spacer()
+                        Text("Score: " + elements[5])
+                    }
+                }
+                Spacer()
+                HStack {
+                    Text("Team: " + elements[3])
+                    Spacer()
+                    Text("Score Differential: " + elements[7])
+                }
+                .font(.subheadline)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+            }
+            .padding()
+        }
+        .onTapGesture {
+            print(elements[3])
+        }
     }
 }
--- DiveMeets/DiveMeets/Views/Parsers/EventPageParser.swift
+++ DiveMeets/DiveMeets/Views/Parsers/EventPageParser.swift
@@ -10,46 +10,41 @@ import SwiftSoup
 
 final class EventPageHTMLParser: ObservableObject {
     //                             Place  Name   NameLink  Team  TeamLink Score ScoreLink Score Diff.
-    @Published var eventPageData = [Int: (String, String, String, String, Double, String, String)]()
-    @Published var parsingPageData = [Int: (String, String, String, String, Double, String, String)]()
+    @Published var eventPageData = [[String]]()
+    @Published var parsingPageData = [[String]]()
 
     let getTextModel = GetTextAsyncModel()
 
-    func parseEventPage(html: String) async throws -> [Int: (String, String, String, String, Double, String, String)] {
+    func parseEventPage(html: String) async throws -> [[String]]{
         let document: Document = try SwiftSoup.parse(html)
         guard let body = document.body() else {
-            return [:]
+            return []
         }
-
-        var beginning = "https://secure.meetcontrol.com/divemeets/system/"
-        var name = ""
+        
+        let beginning = "https://secure.meetcontrol.com/divemeets/system/"
         var partNameLink = ""
-        var nameLink = ""
-        var team = ""
         var partTeamLink = ""
-        var teamLink = ""
-        var score = 0.0
         var partScoreLink = ""
-        var scoreLink = ""
-        var scoreDifference = ""
         
         let table = try body.getElementsByTag("table")
         let overall = try table[0].getElementsByTag("tr")
         for (i, t) in overall.enumerated(){
             if 5 <= i && i < overall.count - 1 {
+                var tempList: [String] = []
                 let line = try t.getElementsByTag("td")
-                name = try line[0].text()
+                tempList.append(String(i - 4))
+                tempList.append(try line[0].text())
                 partNameLink = try line[0].getElementsByTag("a").attr("href")
-                nameLink = beginning + partNameLink
-                team = try line[1].text()
+                tempList.append(beginning + partNameLink)
+                tempList.append(try line[1].text())
                 partTeamLink = try line[1].getElementsByTag("a").attr("href")
-                teamLink = beginning + partTeamLink
-                score = Double(try line[3].text())!
+                tempList.append(beginning + partTeamLink)
+                tempList.append(try line[3].text())
                 partScoreLink = try line[3].getElementsByTag("a").attr("href")
-                scoreLink = beginning + partScoreLink
-                scoreDifference = try line[4].text()
-                await MainActor.run { [name, nameLink, team, teamLink, score, scoreLink, scoreDifference] in
-                    parsingPageData[i - 4] = (name, nameLink, team, teamLink, score, scoreLink, scoreDifference)
+                tempList.append(beginning + partScoreLink)
+                tempList.append(try line[4].text())
+                await MainActor.run { [tempList] in
+                    parsingPageData.append(tempList)
                 }
             }
         }
