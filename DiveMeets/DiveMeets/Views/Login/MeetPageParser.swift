c5a4dd06348ef06e2efa748f6babecc674d54dd8
Logan Sherwin <lsherwin10@gmail.com>
2023-05-29 22:16:44 -0500

    Fix formatting inconsistencies

--- DiveMeets/DiveMeets/Views/Login/MeetPageParser.swift
+++ DiveMeets/DiveMeets/Views/Login/MeetPageParser.swift
@@ -424,8 +424,10 @@ class MeetPageParser: ObservableObject {
         let tables = try content.getElementsByTag("table")
         
         if (link.contains("meetinfo")) {
+            print("Parsing info link...")
             return parseInfoPage(tables: tables)
         } else if (link.contains("meetresults")) {
+            print("Parsing results link...")
             return parseResultsPage(tables: tables)
         }
         print("Failed")
