40bdc33db501dd6d3c15dc1a12f49dc719fd7a74
Logan Sherwin <lsherwin10@gmail.com>
2023-05-29 19:33:23 -0500

    Parse info page events

--- DiveMeets/DiveMeets/Views/Login/MeetPageView.swift
+++ DiveMeets/DiveMeets/Views/Login/MeetPageView.swift
@@ -13,15 +13,34 @@ import SwiftSoup
 
 struct MeetPageView: View {
     @State private var meetData: MeetPageData?
+    @State private var meetEventData: MeetEventData?
     @ObservedObject private var mpp: MeetPageParser = MeetPageParser()
     private let getTextModel = GetTextAsyncModel()
     var meetLink: String
     
+    private func tupleToList(data: MeetEventData) -> [[String]] {
+        var result: [[String]] = []
+        for event in data {
+            let date = event.0
+            let number = String(event.1)
+            let name = event.2
+            let rule = event.3
+            let entries = String(event.4)
+            result.append([date, number, name, rule, entries])
+        }
+        
+        return result
+    }
+    
     var body: some View {
         ZStack {
-            if meetData != nil {
-                ForEach((meetData ?? [:]).sorted(by: >), id: \.key) { key, value in
-                    Text(key + ": " + value)
+            VStack {
+                ForEach(tupleToList(data: meetEventData ?? []), id: \.self) { elem in
+                    Text("Date: " + elem[0])
+                    Text("Event: " + elem[1])
+                    Text("Name: " + elem[2])
+                    Text("Rule: " + elem[3])
+                    Text("Entries: " + elem[4])
                 }
             }
         }
@@ -35,6 +54,9 @@ struct MeetPageView: View {
                 
                 if let html = getTextModel.text {
                     meetData = try await mpp.parseMeetPage(link: meetLink, html: html)
+                    if meetData != nil {
+                        meetEventData = await mpp.getEventData(data: meetData!)
+                    }
                 }
             }
         }
