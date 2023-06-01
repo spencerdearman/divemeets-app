ccdb52e8845f72bb21f2d1ee5e2592f963075365
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-06-01 13:49:18 -0400

    Fixing errors in JSON

--- DiveMeets/DiveMeets/Views/ListOptimizer/ListOptimizer.swift
+++ DiveMeets/DiveMeets/Views/ListOptimizer/ListOptimizer.swift
@@ -6,10 +6,46 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ListOptimizer: View {
-
     var body: some View {
-        Text("List Optimizer View")
+        // Call the function with a key and display the retrieved information
+        Text(lookupInformation(forKey: "101A") ?? "No information found")
+            .padding()
+    }
+    
+    func lookupInformation(forKey key: String) -> String? {
+        if let url = Bundle.main.url(forResource: "diveTable", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                
+                let decodedData = try decoder.decode(YourDataStructure.self, from: data)
+                
+                if let diveData = decodedData.data[key] {
+                    let name = diveData.name
+                    let difficulty = diveData.dd["1"] // Replace "1" with the desired difficulty level
+                    
+                    return "Name: \(name), Difficulty: \(difficulty ?? 0.0)"
+                } else {
+                    return nil
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }
+        
+        return nil
     }
 }
+
+struct YourDataStructure: Codable {
+    struct DiveData: Codable {
+        let name: String
+        let dd: [String: Double]
+    }
+    
+    let data: [String: DiveData]
+}
+
