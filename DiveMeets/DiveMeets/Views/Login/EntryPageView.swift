d8b4753c5b1c884b247a6a62a6d99d898db9dc83
Logan Sherwin <lsherwin10@gmail.com>
2023-06-08 15:00:11 -0400

    Create nicer bubbles, bodies need updated

--- DiveMeets/DiveMeets/Views/Login/EntryPageView.swift
+++ DiveMeets/DiveMeets/Views/Login/EntryPageView.swift
@@ -8,18 +8,29 @@
 import SwiftUI
 
 struct EntryPageView: View {
+    @Environment(\.colorScheme) var currentMode
     @State var entries: [EventEntry]?
     @ObservedObject var ep: EntriesParser = EntriesParser()
     private let getTextModel = GetTextAsyncModel()
+    private var grayColor: Color {
+        currentMode == .light ? Color(red: 0.9, green: 0.9, blue: 0.9) : Color(red: 0.1, green: 0.1, blue: 0.1)
+    }
     
     var body: some View {
         ZStack {
+            grayColor
             if entries != nil {
                 ScrollView(showsIndicators: false) {
-                    VStack {
+                    VStack(spacing: 0) {
                         ForEach(entries!, id: \.self) { entry in
-                            EntryView(entry: entry)
-                                .padding()
+                            ZStack {
+                                Rectangle()
+                                    .fill(.white)
+                                    .cornerRadius(15)
+                                EntryView(entry: entry)
+                                    .padding()
+                            }
+                            .padding()
                         }
                     }
                 }
@@ -27,7 +38,7 @@ struct EntryPageView: View {
         }
         .onAppear {
             Task {
-                let link = "https://secure.meetcontrol.com/divemeets/system/divesheetext.php?meetnum=9032&eventnum=450&eventtype=9"
+                let link = "https://secure.meetcontrol.com/divemeets/system/divesheetext.php?meetnum=9032&eventnum=1030&eventtype=9"
                 // Initialize meet parse from index page
                 let url = URL(string: link)!
                 
@@ -35,7 +46,7 @@ struct EntryPageView: View {
                 await getTextModel.fetchText(url: url)
                 
                 if let html = getTextModel.text {
-                    entries = try await ep.parseEntries(html: html)!
+                    entries = try await ep.parseEntries(html: html)
                 }
                 
             }
@@ -45,18 +56,35 @@ struct EntryPageView: View {
 
 struct EntryView: View {
     var entry: EventEntry
+    @State var isExpanded: Bool = false
+    
+    private func getHeaderString(_ entry: EventEntry) -> String {
+        let last = entry.lastName ?? ""
+        let first = entry.firstName ?? ""
+        let team = entry.team ?? ""
+        return last + ", " + first + " (" + team + ")"
+    }
     
     var body: some View {
-        HStack(alignment: .top) {
-            Text((entry.lastName ?? "") + ", " + (entry.firstName ?? ""))
-            VStack {
-                ForEach(entry.dives ?? [], id: \.self) { dive in
-                    Text(dive.number)
+        DisclosureGroup(
+            isExpanded: $isExpanded,
+            content: {
+                HStack {
+                    VStack {
+                        ForEach(entry.dives ?? [], id: \.self) { dive in
+                            Text(dive.number)
+                        }
+                    }
+                    Text(String(entry.totalDD ?? 0.0))
+                    Text(entry.board ?? "")
                 }
+            },
+            label: {
+                Text(getHeaderString(entry))
+                    .font(.headline)
+                    .foregroundColor(Color.primary)
             }
-            Text(String(entry.totalDD ?? 0.0))
-            Text(entry.board ?? "")
-        }
+        )
     }
 }
 
