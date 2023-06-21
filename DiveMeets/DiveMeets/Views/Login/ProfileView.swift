6be252c8e2ebdfa000e94005b690c3ad664c72f1
Logan Sherwin <lsherwin10@gmail.com>
2023-06-21 12:50:30 -0400

    Fix profile name parsing to only take last word as last name

--- DiveMeets/DiveMeets/Views/Login/ProfileView.swift
+++ DiveMeets/DiveMeets/Views/Login/ProfileView.swift
@@ -95,9 +95,10 @@ struct ProfileView: View {
                     VStack {
                         VStack(alignment: .leading) {
                             HStack (alignment: .firstTextBaseline) {
-                                let firstName = diverData[0][0].slice(from: "Name: ", to: " ") ?? ""
-                                let lastName =
-                                diverData[0][0].slice(from: firstName + " ", to: " ") ?? ""
+                                let nameComps = diverData[0][0].slice(from: "Name: ", to: " State:")?
+                                    .components(separatedBy: " ")
+                                let firstName = nameComps?.dropLast().joined(separator: " ") ?? ""
+                                let lastName = nameComps?.last ?? ""
                                 
                                 diverData != []
                                 ? Text(firstName + " " + lastName) .font(.title)
