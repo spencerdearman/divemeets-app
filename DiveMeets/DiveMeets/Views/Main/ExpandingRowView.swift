3a9c84127bcdab631324bb488bc2fb460823f708
Logan Sherwin <lsherwin10@gmail.com>
2023-06-24 19:55:51 -0400

    RecordList shows all result values and collapse animation appears correctly

--- DiveMeets/DiveMeets/Views/Main/ExpandingRowView.swift
+++ DiveMeets/DiveMeets/Views/Main/ExpandingRowView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ExpandingRowView: View {
     @Namespace var namespace
     @State var show = false
+    @State var pressed = false
+    @State var list = ["Hello", "My", "Name", "Is", "Logan"]
     
     var body: some View {
         GeometryReader { g in
@@ -61,6 +63,45 @@ struct ExpandingRowView: View {
                                         .font(.system(size: 22))
                                 }
                                 .padding()
+                                .background(.white)
+                                .cornerRadius(30)
+                                .matchedGeometryEffect(id: "row", in: namespace)
+                                .frame(height: 100)
+                                .padding()
+                            }
+                            .onTapGesture {
+                                Task {
+                                    await animate(duration: 2, animation: .spring(response: 2,
+                                                                                  dampingFraction: 0.9)) {
+                                        show.toggle()
+                                    }
+                                    pressed = true
+                                }
+                            }
+                        } else {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 30)
+                                    .foregroundColor(.white)
+                                    .matchedGeometryEffect(id: "rect", in: namespace)
+                                    .frame(height: g.size.height + 50)
+                                if pressed {
+                                    let link = "https://secure.meetcontrol.com/divemeets/system/profile.php?number=56961"
+                                    ZStack(alignment: .topLeading) {
+                                        ProfileView(profileLink: link)
+                                        .matchedGeometryEffect(id: "row", in: namespace)
+                                        Button(action: {
+                                            withAnimation(.spring(response: 0.6,
+                                                                  dampingFraction: 0.9)) {
+                                                show.toggle()
+                                                pressed = false
+                                            }
+                                        }) {
+                                            Image(systemName: "chevron.left")
+                                                .font(.system(size: 22))
+                                        }
+                                        .padding()
+                                    }
+                                }
                             }
                         }
                     }
@@ -68,11 +109,5 @@ struct ExpandingRowView: View {
             }
         }
         //        .ignoresSafeArea()
-    }
-}
-
-struct ExpandingRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        ExpandingRowView()
     }
 }
