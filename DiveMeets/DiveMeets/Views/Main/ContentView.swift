77de50c8aac58b9607422f2e1de3cdfcfcf96b34
Logan Sherwin <lsherwin10@gmail.com>
2023-06-24 13:32:51 -0400

    Remove comments, update ContentView

--- DiveMeets/DiveMeets/Views/Main/ContentView.swift
+++ DiveMeets/DiveMeets/Views/Main/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @Environment(\.scenePhase) var scenePhase
     @Environment(\.meetsDB) var db
     @EnvironmentObject private var p: MeetParser
-    @State private var selectedTab: Tab = .house
+    @State private var selectedTab: Tab = .gearshape
     @State var isIndexingMeets: Bool = false
     @FetchRequest(sortDescriptors: []) private var meets: FetchedResults<DivingMeet>
     
@@ -46,8 +46,8 @@ struct ContentView: View {
                                 case .house:
                                     Home()
                                 case .gearshape:
-                                    LiveResultsView(request: "debug")
                                     //ToolsMenu()
+                                    ExpandingRowView()
                                 case .magnifyingglass:
                                     SearchView(isIndexingMeets: $isIndexingMeets)
                                 case .person:
