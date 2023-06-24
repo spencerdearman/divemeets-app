791b13481e1dda27f8431a6aca0eaee25ef01dd9
Logan Sherwin <lsherwin10@gmail.com>
2023-06-24 13:32:51 -0400

    Remove comments, update ContentView

--- DiveMeets/DiveMeets/Views/Search/RecordList.swift
+++ DiveMeets/DiveMeets/Views/Search/RecordList.swift
@@ -54,7 +54,7 @@ struct RecordList: View {
                                         .onDisappear {
                                             resultSelected = true
                                         }
-                                        .onAppear{
+                                        .onAppear {
                                             resultSelected = false
                                         }
                                     }
