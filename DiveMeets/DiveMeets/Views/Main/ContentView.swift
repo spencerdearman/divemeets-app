2f7f324e3c21b4ad864fbd23bdc3acdccebd9b4d
Logan Sherwin <lsherwin10@gmail.com>
2023-06-24 21:26:40 -0400

    Add splash screen file and integrate with ContentView

--- DiveMeets/DiveMeets/Views/Main/ContentView.swift
+++ DiveMeets/DiveMeets/Views/Main/ContentView.swift
@@ -14,7 +14,10 @@ struct ContentView: View {
     @EnvironmentObject private var p: MeetParser
     @State private var selectedTab: Tab = .wrench
     @State var isIndexingMeets: Bool = false
+    @State var showSplash: Bool = true
     @FetchRequest(sortDescriptors: []) private var meets: FetchedResults<DivingMeet>
+    
+    private let splashDuration: CGFloat = 2
     
     var hasHomeButton: Bool {
         if #available(iOS 13.0, *) {
@@ -37,72 +40,85 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            VStack {
-                TabView(selection: $selectedTab) {
-                    ForEach(Tab.allCases, id: \.rawValue) { tab in
-                        HStack {
-                            // Add different page views here for different tabs
-                            switch tab {
-                                case .house:
-                                    Home()
-                                case .wrench:
-                                    NavigationView {
-                                        //ColorfulView()
-                                        LiveResultsView(request: "debug")
-                                    }
-                                    //ToolsMenu()
-                                case .magnifyingglass:
-                                    SearchView(isIndexingMeets: $isIndexingMeets)
-                                case .person:
-                                    LoginSearchView()
+            if showSplash {
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + splashDuration) {
+                            withAnimation {
+                                showSplash = false
                             }
                         }
-                        .tag(tab)
+                    }
+            } else {
+                ZStack {
+                    VStack {
+                        TabView(selection: $selectedTab) {
+                            ForEach(Tab.allCases, id: \.rawValue) { tab in
+                                HStack {
+                                    // Add different page views here for different tabs
+                                    switch tab {
+                                        case .house:
+                                            Home()
+                                        case .wrench:
+                                            NavigationView {
+                                                //ColorfulView()
+                                                LiveResultsView(request: "debug")
+                                            }
+                                            //ToolsMenu()
+                                        case .magnifyingglass:
+                                            SearchView(isIndexingMeets: $isIndexingMeets)
+                                        case .person:
+                                            LoginSearchView()
+                                    }
+                                }
+                                .tag(tab)
+                                
+                            }
+                        }
+                    }
+                    
+                    FloatingMenuBar(selectedTab: $selectedTab)
+                        .offset(y: menuBarOffset)
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                        .dynamicTypeSize(.medium ... .xxxLarge)
+                }
+                .ignoresSafeArea(.keyboard)
+                // Executes on app launch
+                .onAppear {
+                    // isIndexingMeets is set to false by default so it is only executed from start
+                    //     to finish one time (allows indexing to occur in the background without
+                    //     starting over)
+                    if !isIndexingMeets {
+                        isIndexingMeets = true
                         
+                        // Runs this task asynchronously so rest of app can function while this finishes
+                        Task {
+                            // This sets p's upcoming, current, and past meets fields
+                            try await p.parseMeets(storedMeets: meets)
+                            
+                            // Check that each set of meets is not nil and add each to the database
+                            if let upcoming = p.upcomingMeets {
+                                db.addRecords(records: db.dictToTuple(dict: upcoming), type: .upcoming)
+                            }
+                            if let current = p.currentMeets {
+                                db.addRecords(records: db.dictToTuple(dict: current), type: .current)
+                            }
+                            if let past = p.pastMeets {
+                                db.addRecords(records: db.dictToTuple(dict: past), type: .past)
+                            }
+                            
+                            isIndexingMeets = false
+                        }
                     }
                 }
-            }
-            
-            FloatingMenuBar(selectedTab: $selectedTab)
-                .offset(y: menuBarOffset)
-                .frame(maxHeight: .infinity, alignment: .bottom)
-                .dynamicTypeSize(.medium ... .xxxLarge)
-        }
-        .ignoresSafeArea(.keyboard)
-        // Executes on app launch
-        .onAppear {
-            // isIndexingMeets is set to false by default so it is only executed from start
-            //     to finish one time (allows indexing to occur in the background without
-            //     starting over)
-            if !isIndexingMeets {
-                isIndexingMeets = true
-                
-                // Runs this task asynchronously so rest of app can function while this finishes
-                Task {
-                    // This sets p's upcoming, current, and past meets fields
-                    try await p.parseMeets(storedMeets: meets)
-                    
-                    // Check that each set of meets is not nil and add each to the database
-                    if let upcoming = p.upcomingMeets {
-                        db.addRecords(records: db.dictToTuple(dict: upcoming), type: .upcoming)
+                // Executes when other views are opened (notification center, control center, swiped up)
+                .onChange(of: scenePhase) { newPhase in
+                    if newPhase == .active {
+                        GlobalCaches.loadAllCaches()
+                    } else if scenePhase == .active && newPhase == .inactive {
+                        GlobalCaches.saveAllCaches()
                     }
-                    if let current = p.currentMeets {
-                        db.addRecords(records: db.dictToTuple(dict: current), type: .current)
-                    }
-                    if let past = p.pastMeets {
-                        db.addRecords(records: db.dictToTuple(dict: past), type: .past)
-                    }
-                    
-                    isIndexingMeets = false
                 }
-            }
-        }
-        // Executes when other views are opened (notification center, control center, swiped up)
-        .onChange(of: scenePhase) { newPhase in
-            if newPhase == .active {
-                GlobalCaches.loadAllCaches()
-            } else if scenePhase == .active && newPhase == .inactive {
-                GlobalCaches.saveAllCaches()
             }
         }
     }
---
+++ DiveMeets/DiveMeets/Views/Main/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  DiveMeets
+//
+//  Created by Logan Sherwin on 6/24/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
