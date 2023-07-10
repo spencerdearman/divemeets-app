3cde45fca4cfe769a197ae487816e30c8595e98d
Logan Sherwin <lsherwin10@gmail.com>
2023-07-10 00:07:04 -0400

    Refactor parseEventPage function for improved readability

--- DiveMeets/DiveMeets/Parsers/EventPageParser.swift
+++ DiveMeets/DiveMeets/Parsers/EventPageParser.swift
@@ -13,7 +13,8 @@ final class EventPageHTMLParser: ObservableObject {
     @Published var eventPageData = [[String]]()
     @Published var parsingPageData = [[String]]()
     
-    let getTextModel = GetTextAsyncModel()
+    private let getTextModel = GetTextAsyncModel()
+    private let leadingLink = "https://secure.meetcontrol.com/divemeets/system/"
     
     func parseEventPage(html: String) async throws -> [[String]]{
         let document: Document = try SwiftSoup.parse(html)
@@ -21,32 +22,26 @@ final class EventPageHTMLParser: ObservableObject {
             return []
         }
         
-        let beginning = "https://secure.meetcontrol.com/divemeets/system/"
-        var partNameLink = ""
-        var partTeamLink = ""
-        var partScoreLink = ""
-        
         let table = try body.getElementsByTag("table")
         let overall = try table[0].getElementsByTag("tr")
         for (i, t) in overall.enumerated(){
             if 5 <= i && i < overall.count - 1 {
-                var tempList: [String] = []
                 let line = try t.getElementsByTag("td")
-                tempList.append(String(i - 4))
-                tempList.append(try line[0].text())
-                partNameLink = try line[0].getElementsByTag("a").attr("href")
-                tempList.append(beginning + partNameLink)
-                tempList.append(try line[1].text())
-                partTeamLink = try line[1].getElementsByTag("a").attr("href")
-                tempList.append(beginning + partTeamLink)
-                tempList.append(try line[3].text())
-                partScoreLink = try line[3].getElementsByTag("a").attr("href")
-                tempList.append(beginning + partScoreLink)
-                tempList.append(try line[4].text())
-                tempList.append(try overall[2].text())
                 
-                await MainActor.run { [tempList] in
-                    parsingPageData.append(tempList)
+                let place = String(i - 4)
+                let name = try line[0].text()
+                let nameLink = try leadingLink + line[0].getElementsByTag("a").attr("href")
+                let team = try line[1].text()
+                let teamLink = try leadingLink + line[1].getElementsByTag("a").attr("href")
+                let score = try line[3].text()
+                let scoreLink = try leadingLink + line[3].getElementsByTag("a").attr("href")
+                let scoreDiff = try line[4].text()
+                let eventName = try overall[2].text()
+                
+                let items = [place, name, nameLink, team, teamLink, score, scoreLink, scoreDiff, eventName]
+                
+                await MainActor.run { [items] in
+                    parsingPageData.append(items)
                 }
             }
         }
