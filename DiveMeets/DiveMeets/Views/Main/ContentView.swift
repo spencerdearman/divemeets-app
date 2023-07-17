bf35ecb0474b581aec326bd97ebeaa182e67b604
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-07-17 10:41:42 -0500

    Update DiveMeets/DiveMeets/Views/Main/ContentView.swift
    Co-authored-by: Logan Sherwin <41316433+lsherwin10@users.noreply.github.com>

--- DiveMeets/DiveMeets/Views/Main/ContentView.swift
+++ DiveMeets/DiveMeets/Views/Main/ContentView.swift
@@ -69,8 +69,8 @@ struct ContentView: View {
                                     case .house:
                                         Home()
                                     case .wrench:
-                                        NavigationView {
-                                            LiveResultsView(request: "debug")
+                                        //NavigationView {
+                                            //LiveResultsView(request: "debug")
 //                                            FinishedLiveResultsView(link: "https://secure.meetcontrol.com/divemeets/system/livestats.php?event=stats-9050-770-9-Finished")
                                         //}
                                         //.navigationViewStyle(StackNavigationViewStyle())
